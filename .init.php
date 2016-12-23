b2a890aca0bd24f7933adde59fb9b6c01cd6bdb3
Rick Deckard <xxxblasteroid@gmail.com>
2016-12-23 14:18:05 +0300

    fix

--- .init.php
+++ .init.php
@@ -7,11 +7,14 @@
 
 namespace Rodzeta\Importrealtyfeed;
 
+use Bitrix\Main\Web\HttpClient;
+
 const ID = "rodzeta.importrealtyfeed";
 const APP = __DIR__ . "/";
 const LIB = APP  . "lib/";
 
 define(__NAMESPACE__ . "\LOG", $_SERVER["DOCUMENT_ROOT"]  . "/upload/.log_" . ID);
+define(__NAMESPACE__ . "\FILE_FEED", $_SERVER["DOCUMENT_ROOT"]  . "/upload/." . ID . "_file1.xml");
 
 function Log($data) {
 	file_put_contents(LOG, date("Y-m-d H:i:s") . "\t" . print_r($data, true) . "\n", FILE_APPEND);
@@ -22,7 +25,17 @@ function ImportChunk($xmlFile, $start = 0) {
 }
 
 function Import() {
-	// TODO use config
+	// TODO get from options
+	$currentOptions = [
+		"src_url" => "http://anton.citrus-dev.ru/import.xml",
+	];
+
+	if (!file_exists(FILE_FEED)) {
+		// fetch feed
+		$client = new HttpClient();
+		$client->download($currentOptions["src_url"], FILE_FEED);
+	}
+
 	// run import chunk
 	Log("start next chunk");
 
